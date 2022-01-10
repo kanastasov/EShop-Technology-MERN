dafe93d7eae5dad0e6c86d9ace94bfaeda074636
kanastasov <kirilanastasoff@gmail.com>
2022-01-10 12:21:11 +0200

    first commit MERN

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className = 'py-3'>
+      <Container>
+          <h1> Welcome to E Shop! </h1>
+      </Container>
+     
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
