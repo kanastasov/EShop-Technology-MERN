9146ad43f39967a8df07f4fbf13bef17f78ff93d
kanastasov <kirilanastasoff@gmail.com>
2022-01-11 17:46:05 +0200

    started with node back end

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const products = require('./data/products')
+// import {Link,useParams } from 'react-router-dom'
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.send('API is running...');
+})
+
+app.get('/api/products', (req,res) =>{
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req,res) =>{
+    // const product = products.find((p) => p._id === req.params.id)
+
+//  const { id} = useParams();
+ 
+const product = products.find((p) => p._id === match.params.id)
+// const product = products.find((p) => p._id === (id));
+
+
+    res.json(product)
+})
+
+
+app.listen(5000, console.log('Server running on port 5000'))
