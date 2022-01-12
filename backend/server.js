a614beab6ffde6997218f78778b417afe0b4dc4d
kanastasov <kirilanastasoff@gmail.com>
2022-01-12 10:11:32 +0200

    nodemon and concurrently set up

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const products = require('./data/products')
 const app = express()
 
 app.get('/', (req,res) =>{
-    res.send('API is running...');
+    res.send('API is running....');
 })
 
 app.get('/api/products', (req,res) =>{
