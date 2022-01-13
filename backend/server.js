cd8ec541d830591dd0776d9cee720263c3165f34
kanastasov <kirilanastasoff@gmail.com>
2022-01-13 10:54:26 +0200

    Set up the MongoDb cluster and DB with compass

--- backend/server.js
+++ backend/server.js
@@ -29,3 +29,5 @@ const product = products.find((p) => p._id === req.params.id)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` ))
+
+// added MongoDB cluster and db with compas
