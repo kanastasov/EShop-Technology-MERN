1ecee08975e5882e427252ba85a58a7a569a77bc
kanastasov <kirilanastasoff@gmail.com>
2022-01-17 13:13:47 +0200

    Cart Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,14 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer,productDetailReducer } from './reducers/productReducers';
-
+import {cartReducer} from './reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailReducer,
+    cart: cartReducer
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage}
+};
 
 const middleware = [thunk];
 
