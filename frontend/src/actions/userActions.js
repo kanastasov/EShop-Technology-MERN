39183c3a7dbaed626001c52033cd91d592cf3305
kanastasov <kirilanastasoff@gmail.com>
2022-01-19 17:56:34 +0200

    fixed user authentication

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,34 +1,41 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from './../constants/userConstants';
-import { axios } from 'axios';
+import axios from 'axios'
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from '../constants/userConstants'
 
-export const login = (email, password) => async(dispatch) => {
-    try {
-        dispatch({
-            type: USER_LOGIN_REQUEST
-        })
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    })
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-
-        const{data} = await axios.post('/api/users/login',{email,password},config)
-        
-        dispatch({
-            type: USER_LOGIN_SUCCESS
-        })
-
-        localStorage.setItem('userInfo', JSON.stringify(data))
-
-
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-                error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-            });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
+
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    )
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
