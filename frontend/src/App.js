c76424a621ac526e731711e69ad20840fc2e756c
kanastasov <kirilanastasoff@gmail.com>
2022-01-22 09:33:40 +0200

    Shipping screen & save address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
-
+import ShippingScreen from './screens/ShippingScreen';
 const App = () => {
   return (
     <Router>
@@ -20,6 +20,7 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+             <Route path='/shipping' component={ShippingScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
