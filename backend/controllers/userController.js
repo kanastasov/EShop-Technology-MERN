085536809707dc2a572982bac2555a0230bd06dd
kanastasov <kirilanastasoff@gmail.com>
2022-01-26 17:41:28 +0200

    admin delete a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -110,8 +110,22 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   res.json(users)
+});
 
+// @desc    Delete user
+// @route   DELETE /api/users/:ID
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.is);
+
+  if(user) {
+    await user.remove()
+     res.json({message: 'User removed'})
+  }else {
+      res.status(404)
+      throw new Error('User not found')
+  }
  
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile,getUsers };
+export { authUser, registerUser, getUserProfile, updateUserProfile,getUsers,deleteUser };
