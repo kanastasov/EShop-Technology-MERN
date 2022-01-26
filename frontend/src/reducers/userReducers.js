8331fd57af1c582291c9722e921fb37e2949baf6
kanastasov <kirilanastasoff@gmail.com>
2022-01-26 16:04:28 +0200

    admin screen security

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
@@ -16,6 +17,7 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -82,6 +84,10 @@ export const userListReducer = (state = { users: [] }, action) => {
       return { loading: false, users: action.payload };
     case USER_LIST_FAIL:
       return { loading: false, error: action.payload };
+      case USER_LIST_RESET :
+      return {
+        users:[]
+      }
     default:
       return state;
   }
