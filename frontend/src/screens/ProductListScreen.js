a83b0e2bd50b3b880d6c5a6f4a44a7158cae739e
kanastasov <kirilanastasoff@gmail.com>
2022-01-27 12:52:22 +0200

    admin delete product

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,22 +1,26 @@
 import React, { useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Table, Button, Row,Col } from 'react-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 
-const ProductListScreen = ({ history,match }) => {
+const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch()
 
   const productList = useSelector((state) => state.productList)
   const { loading, error, products } = productList
 
+  const productDelete = useSelector((state) => state.productDelete)
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete
+
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
-    const createProductHandler = () => {
-        console.log('createProductHandler')
-    }
 
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
@@ -24,29 +28,32 @@ const ProductListScreen = ({ history,match }) => {
     } else {
       history.push('/login')
     }
-  }, [dispatch, history, userInfo])
+  }, [dispatch, history, userInfo, successDelete])
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure')) {
-      //dispatch(deleteUser(id))
+      dispatch(deleteProduct(id))
     }
+  }
+
+  const createProductHandler = (product) => {
+    //   CREATE PRODUCT
   }
 
   return (
     <>
-        <Row className='align-items-center'>
-            <Col>
-                <h1>Products</h1>
-            </Col>
-
-                <Col className='text-right'>
-                 <Button 
-                    className='my-3'
-                    onClick={createProductHandler}
-                 ><i className='fas fa-plus'></i>Create Product</Button>
-                </Col>
-        </Row>
-
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fas fa-plus'></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
@@ -68,13 +75,9 @@ const ProductListScreen = ({ history,match }) => {
               <tr key={product._id}>
                 <td>{product._id}</td>
                 <td>{product.name}</td>
-                <td>
-                    ${product.price}
-                </td>
-                <td>
-                    {product.category}
-                </td>
-                <td>{product.brand} </td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
                 <td>
                   <LinkContainer to={`/admin/product/${product._id}/edit`}>
                     <Button variant='light' className='btn-sm'>
