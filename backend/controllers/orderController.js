2637c7709edecd3335d1568b5170d62904dbd2e3
kanastasov <kirilanastasoff@gmail.com>
2022-01-28 13:30:09 +0200

    mark order as delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,6 +79,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
@@ -95,5 +114,8 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
+
+
+
 export { addOrderItems, getOrderById, 
-updateOrderToPaid, getMyOrders,getOrders }
+updateOrderToPaid, getMyOrders,getOrders,updateOrderToDelivered }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -6,6 +6,7 @@ import {
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToDelivered,
 } from '../controllers/orderController.js'
 import { protect,admin } from '../middleware/authMiddleware.js'
 
@@ -13,5 +14,7 @@ router.route('/').post(protect, addOrderItems).get(protect,admin,getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
+router.route('/:id/deliver').put(protect,admin, updateOrderToDelivered)
+
 
 export default router
