aa4b21aa5f89a4ee38174d2680702aa0ec218764
kanastasov <kirilanastasoff@gmail.com>
2022-01-28 15:58:14 +0200

    Added morgan library and review endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,12 +6,15 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from '../controllers/productController.js'
 
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+
 router.route('/:id').get(getProductById)
 .delete(protect, admin, deleteProduct)
 .put(protect,admin,updateProduct)
