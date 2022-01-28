715e53a3dc5b35c65176bcb7866fa200787a23c5
kanastasov <kirilanastasoff@gmail.com>
2022-01-28 10:21:24 +0200

    edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
   return (
@@ -30,11 +31,13 @@ const App = () => {
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/productList' component={ProductListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productList' component={ProductListScreen} />
+          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+         <Route path='/product/:id' component={ProductScreen} />
+        
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
