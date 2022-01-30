b017b3c359fa15bc0fdcbe880caf4072f65f713f
kanastasov <kirilanastasoff@gmail.com>
2022-01-30 11:46:54 +0200

    prep for prod

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
